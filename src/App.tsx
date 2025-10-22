c6d43423e6318f29863bdef51771d32a38ffa0fa
isalu <isabellalucena15@gmail.com>
2025-10-22 14:19:29 -0300

    explicação do react fragment e criação de componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Usa PascalCase
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR 
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        // (React) Fragment <> </> para não adicionar nós desnecessários na árvore DOM
+        <> 
+            <h1>Olá mundo do APP!</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolorem magni quisquam nisi sed, voluptatem repellat neque unde aspernatur fugit expedita excepturi sunt accusantium ad reprehenderit reiciendis in quae quas.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod aperiam aliquid facere odio repellat quia eum est odit maxime deserunt consectetur eaque natus consequatur blanditiis a dolorum vero, at corrupti?
+            </p>
+        </>
+    );
 }
 
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   // boas práticas usar garante que enquanto a aplicação está em desenvolvimento ela funcione corretamente
   <StrictMode> 
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
