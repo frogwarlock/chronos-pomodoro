2a72d091add492decd375f975d0dd4e9edc90cd1
isalu <isabellalucena15@gmail.com>
2025-10-29 14:37:24 -0300

    feat: criando o menu com os links específicos para o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { Container } from './components/Container';
 import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 import './styles/theme.css'
 import './styles/global.css'
 
@@ -13,7 +14,9 @@ export function App() {
            <Container>
                 <Logo />
            </Container>
-           <Container><Heading>MENU</Heading></Container>
+           <Container>
+                <Menu />
+           </Container>
            <Container><Heading>FORM</Heading></Container>
            <Container><Heading>FOOTER</Heading></Container>
         </>
